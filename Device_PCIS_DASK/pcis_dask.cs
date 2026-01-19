0144cacd8ae54ac50bab5178e6f4799f2e1f4ec3
Leo <leo_li@fittech.com.tw>
2026-01-19 15:05:59 +0800

    測試用

--- Device_PCIS_DASK/pcis_dask.cs
+++ Device_PCIS_DASK/pcis_dask.cs
@@ -17,7 +17,7 @@ namespace Device_PCIS_DASK
                 pCI_Parm.CardType = DASK64.PCI_9111HR;
                 pCI_Parm.Input_Status = new bool[lineMaxCount, devMaxCount, portMaxCount];
             }
-            else if (card_type == "PCI_9111DG")
+            else if (card_type == "PCI_9111DG")  
             {
                 pCI_Parm.CardType = DASK64.PCI_9111DG;
                 pCI_Parm.Input_Status = new bool[lineMaxCount, devMaxCount, portMaxCount];
